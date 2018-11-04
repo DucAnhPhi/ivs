c19554f77bca29a1362d01ef0ed4581c54483796
DucAnhPhi <ducanhphi1@gmail.com>
2018-11-04 17:43:50 +0100

    print computation time for different parallel requests

--- ivs2/ex5/client.py
+++ ivs2/ex5/client.py
@@ -11,38 +11,38 @@ if __name__ == "__main__":
     # Parameters here: server-name/IP-adr, server-port
     c = rpyc.connect("localhost", 18861)
 
-    lower, upper = 2, 300000
-    parallel_requests = 10
-    async_get_primes = rpyc.async_(c.root.get_primes)
+    lower, upper = 1000000, 3000000
     
-    print("\n### Starting request ...")
-    
-    offset = (upper-lower) // parallel_requests
-    temp_lower = lower
-    list_of_primes = []
-    async_results = []
+    for parallel_requests in range(1, 11):
 
-    start = timer()
+        print("\n### Starting request with {} parallel requests...".format(parallel_requests))
 
-    for n in range(1, parallel_requests + 1):
-        temp_upper = temp_lower + offset
-        if n == parallel_requests and temp_upper < upper:
-            temp_upper = upper
-        result = async_get_primes(temp_lower, temp_upper)
-        async_results.append(result)
-        temp_lower = temp_upper
-    
+        async_get_primes = rpyc.async_(c.root.get_primes)
+        offset = (upper-lower) // parallel_requests
+        temp_lower = lower
+        list_of_primes = []
+        async_results = []
 
-    while len(async_results) != 0:
-        for i in range(len(async_results)):
-            if async_results[i].ready:
-                callback()
-                for prime in async_results[i].value:
-                    list_of_primes.append(prime)
-                async_results.pop(i)
-                break
-    
-    end = timer()
-    
-    print("### Received {} primes".format(len(list_of_primes)))
-    print("### Computation took {} seconds".format(end-start))
+        start = timer()
+
+        for req in range(1, parallel_requests + 1):
+            temp_upper = temp_lower + offset
+            if req == parallel_requests and temp_upper < upper:
+                temp_upper = upper
+            result = async_get_primes(temp_lower, temp_upper)
+            async_results.append(result)
+            temp_lower = temp_upper
+
+
+        while len(async_results) != 0:
+            for i in range(len(async_results)):
+                if async_results[i].ready:
+                    callback()
+                    for prime in async_results[i].value:
+                        list_of_primes.append(prime)
+                    async_results.pop(i)
+                    break
+
+        end = timer()
+        print("### Computation for {} parallel requests took {} seconds".format(parallel_requests, end-start))
+        # print("### Received {} primes".format(len(list_of_primes)))
